6bef5ad60760785df33e4dfecf6a68bc4454b652
Grigorii Horos <horosgrisa@gmail.com>
2020-03-18 10:53:34 +0200

    Fix plugin

--- pr-node.plugin.zsh
+++ pr-node.plugin.zsh
@@ -1,46 +1,44 @@
 #!/usr/bin/env zsh
 
-DEPENDENCES_ZSH+=( zpm-zsh/helpers zpm-zsh/colors )
-
-typeset -g NODE_VERSION_PREFIX
-NODE_VERSION_PREFIX=${NODE_VERSION_PREFIX:-" "}
-typeset -g NODE_VERSION_SUFIX
-NODE_VERSION_SUFIX=${NODE_VERSION_SUFIX:-""}
+typeset -g NODE_VERSION_PREFIX=${NODE_VERSION_PREFIX:-" "}
+typeset -g NODE_VERSION_SUFIX=${NODE_VERSION_SUFIX:-""}
+typeset -g pr_node
 
 if (( $+functions[zpm] )); then
-  zpm zpm-zsh/helpers,inline zpm-zsh/colors,inline
+  zpm zpm-zsh/helpers zpm-zsh/colors
 fi
 
-_pr_node() {
-  typeset -g pr_node
-  if (( $+commands[node] )); then
-    if is-recursive-exist package.json >/dev/null ; then
+if (( $+commands[node] )); then
+
+  function _pr_node() {
+    pr_node=""
+
+    if is-recursive-exist package.json >/dev/null; then
       pr_node="$NODE_VERSION_PREFIX"
-      
-      if is-recursive-exist gulpfile.js   >/dev/null || is-recursive-exist gulpfile.babel.js >/dev/null ; then
+
+      if is-recursive-exist gulpfile.js >/dev/null || is-recursive-exist gulpfile.babel.js >/dev/null; then
         pr_node+="%{$c[red]$c_bold$c_dim%}%{$c_reset%} "
       fi
-      
-      if is-recursive-exist Gruntfile.js   >/dev/null ; then
+
+      if is-recursive-exist Gruntfile.js >/dev/null; then
         pr_node+="%{$c[yellow]$c_bold$c_dim%}%{$c_reset%} "
       fi
-      
-      if is-recursive-exist webpack.config.js   >/dev/null ; then
+
+      if is-recursive-exist webpack.config.js >/dev/null; then
         pr_node+="%{$c[blue]$c_bold$c_dim%}ﰩ%{$c_reset%} "
       fi
-      
+
       nodev=$(node -v)
       nodev=${nodev#'v'}
-      
+
       pr_node+="%{$c[green]${c_bold}%}⬢%{$c_reset%} %{$c[blue]$c_bold%}$nodev%{$c_reset%}"
       pr_node+="$NODE_VERSION_SUFIX"
 
       return 0
     fi
-  fi
-  
-  pr_node=""
-}
+  }
 
-_pr_node
-add-zsh-hook chpwd _pr_node
+
+  add-zsh-hook chpwd _pr_node
+  _pr_node
+fi

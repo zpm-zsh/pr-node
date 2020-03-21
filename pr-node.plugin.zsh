1331f1e1baea024569466dd872800f8d751af651
Grigorii Horos <horosgrisa@gmail.com>
2020-03-21 13:15:59 +0200

    Optimize plugin

--- pr-node.plugin.zsh
+++ pr-node.plugin.zsh
@@ -4,27 +4,26 @@ typeset -g NODE_VERSION_PREFIX=${NODE_VERSION_PREFIX:-" "}
 typeset -g NODE_VERSION_SUFIX=${NODE_VERSION_SUFIX:-""}
 typeset -g pr_node
 
-if (( $+functions[zpm] )); then
-  zpm zpm-zsh/helpers zpm-zsh/colors
-fi
+if (( $+functions[zpm] )); then #DO_NOT_INCLUDE_LINE_IN_ZPM_CACHE
+  zpm zpm-zsh/helpers zpm-zsh/colors #DO_NOT_INCLUDE_LINE_IN_ZPM_CACHE
+fi #DO_NOT_INCLUDE_LINE_IN_ZPM_CACHE
 
 if (( $+commands[node] )); then
-
   function _pr_node() {
     pr_node=""
 
-    if is-recursive-exist package.json >/dev/null; then
+    if is-recursive-exist package.json; then
       pr_node="$NODE_VERSION_PREFIX"
 
-      if is-recursive-exist gulpfile.js >/dev/null || is-recursive-exist gulpfile.babel.js >/dev/null; then
+      if is-recursive-exist gulpfile.js || is-recursive-exist gulpfile.babel.js; then
         pr_node+="%{$c[red]$c_bold$c_dim%}%{$c_reset%} "
       fi
 
-      if is-recursive-exist Gruntfile.js >/dev/null; then
+      if is-recursive-exist Gruntfile.js; then
         pr_node+="%{$c[yellow]$c_bold$c_dim%}%{$c_reset%} "
       fi
 
-      if is-recursive-exist webpack.config.js >/dev/null; then
+      if is-recursive-exist webpack.config.js; then
         pr_node+="%{$c[blue]$c_bold$c_dim%}ﰩ%{$c_reset%} "
       fi
 
@@ -33,12 +32,10 @@ if (( $+commands[node] )); then
 
       pr_node+="%{$c[green]${c_bold}%}⬢%{$c_reset%} %{$c[blue]$c_bold%}$nodev%{$c_reset%}"
       pr_node+="$NODE_VERSION_SUFIX"
-
-      return 0
     fi
   }
 
-
+  autoload -Uz add-zsh-hook
   add-zsh-hook chpwd _pr_node
   _pr_node
 fi

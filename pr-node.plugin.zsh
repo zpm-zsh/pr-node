48cc4b9ac815cae4b1ff1838d6b4b4cedc6ea277
Grigorii Horos <horosgrisa@gmail.com>
2020-04-12 22:14:14 +0300

    Optimize

--- pr-node.plugin.zsh
+++ pr-node.plugin.zsh
@@ -13,21 +13,21 @@ if (( $+commands[node] )); then
       pr_node="$NODE_VERSION_PREFIX"
 
       if is-recursive-exist gulpfile.js || is-recursive-exist gulpfile.babel.js; then
-        pr_node+="%{$c[red]$c_bold$c_dim%}%{$c_reset%} "
+        pr_node+="%{${c[red]}${c[bold]}${c[dim]}%}%{${c[reset]}%} "
       fi
 
       if is-recursive-exist Gruntfile.js; then
-        pr_node+="%{$c[yellow]$c_bold$c_dim%}%{$c_reset%} "
+        pr_node+="%{${c[yellow]}${c[bold]}${c[dim]}%}%{${c[reset]}%} "
       fi
 
       if is-recursive-exist webpack.config.js; then
-        pr_node+="%{$c[blue]$c_bold$c_dim%}ﰩ%{$c_reset%} "
+        pr_node+="%{${c[blue]}${c[bold]}${c[dim]}%}ﰩ%{${c[reset]}%} "
       fi
 
       nodev=$(node -v)
       nodev=${nodev#'v'}
 
-      pr_node+="%{$c[green]${c_bold}%}⬢%{$c_reset%} %{$c[blue]$c_bold%}$nodev%{$c_reset%}"
+      pr_node+="%{${c[green]}${c[bold]}%}⬢%{${c[reset]}%} %{${c[blue]}${c[bold]}%}$nodev%{${c[reset]}%}"
       pr_node+="$NODE_VERSION_SUFIX"
     fi
   }

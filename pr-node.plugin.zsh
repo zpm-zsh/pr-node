8b09ea422e8492c543bedfa4e1af27579026e4ae
Grigorii Horos <horosgrisa@gmail.com>
2023-08-18 00:04:08 +0300

    Change ui

--- pr-node.plugin.zsh
+++ pr-node.plugin.zsh
@@ -1,8 +1,6 @@
 #!/usr/bin/env zsh
 
-: ${PR_NODE_GULPFILE:=''}
-: ${PR_NODE_WEBPACK:='ﰩ'}
-: ${PR_NODE_SYMBOL:=''}
+: ${PR_NODE_SYMBOL:='󰎙'}
 
 typeset -g pr_node
 
@@ -11,20 +9,14 @@ if (( $+commands[node] )); then
     pr_node=''
 
     if is-recursive-exist package.json; then
-      pr_node=' '
-
-      if is-recursive-exist gulpfile.js || is-recursive-exist gulpfile.babel.js; then
-        pr_node+="%{${c[red]}${c[bold]}${c[dim]}%}${NODE_GULPFILE}%{${c[reset]}%} "
-      fi
-
-      if is-recursive-exist webpack.config.js; then
-        pr_node+="%{${c[blue]}${c[bold]}${c[dim]}%}ﰩ%{${c[reset]}%} "
-      fi
+      pr_node=" %{${c[base3]}${c[bold]}%}(%{${c[reset]}%}"
 
       nodev=$(node -v)
       nodev=${nodev#'v'}
 
-      pr_node+="%{${c[springgreen]}${c[bold]}%}${PR_NODE_SYMBOL}%{${c[reset]}%} %{${c[blue]}${c[bold]}%}$nodev%{${c[reset]}%}"
+      pr_node+="%{${c[green]}${c[bold]}%}${PR_NODE_SYMBOL}%{${c[reset]}%} %{${c[green]}${c[bold]}%}$nodev%{${c[reset]}%}"
+
+      pr_node+="%{${c[base3]}${c[bold]}%})%{${c[reset]}%}"
     fi
   }
 
